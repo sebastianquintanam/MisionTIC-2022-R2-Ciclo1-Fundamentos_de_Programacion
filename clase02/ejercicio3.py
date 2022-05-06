196eb18629d49ff60fb169f442211124445ec92d
sebastianquintanam <sebastian.quintana.m@gmail.com>
2022-05-06 18:52:51 -0500

    ejercicos

---
+++ clase02/ejercicio3.py
@@ -0,0 +1,9 @@
+# Datos de entrada
+Peces_rojos = 284
+Peces_azules = 163
+
+# Procedimiento
+Total_peces = Peces_rojos + Peces_azules
+
+# Salida
+print("El total de peces es:", Total_peces)
---
+++ clase02/ejercicio4.py
@@ -0,0 +1,11 @@
+# Datos de entrada
+descuento_cine = 0.7
+dinero_carmen = 23
+gasto = 10.25
+ahorros_carmen = 12.75
+
+# Procedimiento
+gasto = dinero_carmen - ahorros_carmen
+
+# Salida
+print("En total gasto", gasto)
